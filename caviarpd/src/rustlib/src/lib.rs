2ce24125ca4ca8c0a6fefc1e3b54e29eaa70aea4
David B. Dahl <dahl@stat.byu.edu>
2021-07-10 19:47:39 -0600

    Latest API

--- caviarpd/src/rustlib/src/lib.rs
+++ caviarpd/src/rustlib/src/lib.rs
@@ -77,9 +77,9 @@ fn sample_epa(
         n_samples,
         n_items,
         similarity.as_double_slice(),
-        mass.as_double_scalar(),
-        discount.as_double_scalar(),
-        usize::try_from(n_cores.as_integer_scalar()).unwrap(),
+        mass.as_f64(),
+        discount.as_f64(),
+        usize::try_from(n_cores.as_i32()).unwrap(),
         &mut rng,
     );
     let n_samples = samples.len() / n_items;
@@ -116,8 +116,8 @@ fn caviarpd_n_clusters(
         n_samples,
         n_items,
         similarity.as_double_slice(),
-        mass.as_double_scalar(),
-        discount.as_double_scalar(),
+        mass.as_f64(),
+        discount.as_f64(),
         n_cores.as_usize(),
         &mut rng,
     );
@@ -132,11 +132,11 @@ fn caviarpd_n_clusters(
     };
     let p = SALSOParameters {
         n_items,
-        max_size: LabelType::try_from(max_size.as_integer_scalar()).unwrap(),
+        max_size: LabelType::try_from(max_size.as_i32()).unwrap(),
         max_size_as_rf: false,
         max_scans: u32::MAX,
         max_zealous_updates: 10,
-        n_runs: u32::try_from(n_runs.as_integer_scalar()).unwrap(),
+        n_runs: u32::try_from(n_runs.as_i32()).unwrap(),
         prob_sequential_allocation: 0.5,
         prob_singletons_initialization: 0.0,
     };
@@ -145,8 +145,8 @@ fn caviarpd_n_clusters(
         loss_function,
         &p,
         f64::INFINITY,
-        u32::try_from(n_cores.as_integer_scalar()).unwrap(),
+        u32::try_from(n_cores.as_i32()).unwrap(),
         &mut rng,
     );
-    r::integer_scalar((fit.clustering.into_iter().max().unwrap() + 1) as i32)
+    r::integer_scalar(i32::try_from(fit.clustering.into_iter().max().unwrap() + 1).unwrap())
 }
